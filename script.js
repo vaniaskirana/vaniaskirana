c3dc0ab60925c9186bbafeea77b833aadc7abac0
VaniaSasikirana <sk080501@gmail.com>
2022-09-28 20:32:15 +0700

    presentasi 1

---
+++ script.js
@@ -0,0 +1,24 @@
+// // for(let bilangan=1; bilangan <=100; bilangan++){
+// //     console.log(bilangan);
+
+// // }
+
+// // for(let permen=10; permen>=1; permen--){
+// //     console.log(permen);
+// // }
+
+// let bilangan1 = 10;
+// let bilangan2 = 3;
+
+// console.log(bilangan1 * bilangan2); // Output: 30
+
+
+let ongkir = 30000;
+
+if (ongkir > 25000) {
+console.log("Gratis Ongkir");
+} else if (ongkir >= 15000 && ongkir <= 25000) {
+console.log("Ongkir dipotong 8000");
+} else {
+console.log("Mohon maaf anda belum mendapatkan diskon ongkir.");
+}
